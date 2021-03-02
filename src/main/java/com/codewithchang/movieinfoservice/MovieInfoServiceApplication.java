59df86ce5d6b350f71a7951ad224f3e7c682dc1f
Chang <cnxiong03@gmail.com>
2021-03-01 22:25:37 -0500

    movie info service

---
+++ src/main/java/com/codewithchang/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,14 @@
+package com.codewithchang.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	//Movie Info Service gets the movie information
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
